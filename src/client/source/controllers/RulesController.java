d223df9603179396430d304aa3d958dd3047a24f
Robin Silvério <robinsilverio17@gmail.com>
2018-05-25 11:02:02 +0200

    example of a scrollpane for displaying rules

--- src/client/source/controllers/RulesController.java
+++ src/client/source/controllers/RulesController.java
@@ -1,4 +1,26 @@
 package client.source.controllers;
 
-public class RulesController {
+import javafx.event.ActionEvent;
+import javafx.fxml.FXML;
+import javafx.fxml.Initializable;
+import javafx.scene.image.Image;
+import javafx.scene.image.ImageView;
+
+import java.io.File;
+import java.net.URL;
+import java.util.ResourceBundle;
+
+public class RulesController implements Initializable {
+
+    @FXML private ImageView firstSideRules;
+
+    /**
+     * This is for loading images to imageViews
+     *  @param location
+     *  @param resources
+     * */
+    @Override
+    public void initialize(URL location, ResourceBundle resources) {
+        firstSideRules.setImage(new Image("client/resources/img/rules.jpg"));
+    }
 }
