e1226f3fb9a0565add3c7aed48d0786d886adae8
Robin Silvério <robinsilverio17@gmail.com>
2018-05-25 15:26:39 +0200

    Commencing adding images to tabs

--- src/client/source/controllers/RulesController.java
+++ src/client/source/controllers/RulesController.java
@@ -8,11 +8,27 @@ import javafx.scene.image.ImageView;
 
 import java.io.File;
 import java.net.URL;
+import java.util.ArrayList;
 import java.util.ResourceBundle;
 
 public class RulesController implements Initializable {
 
-    @FXML private ImageView firstSideRules;
+    @FXML private ImageView rules_0;
+    @FXML private ImageView rules_1;
+    @FXML private ImageView rules_2;
+    @FXML private ImageView rules_3;
+    @FXML private ImageView rules_4;
+    @FXML private ImageView rules_5;
+    @FXML private ImageView rules_6;
+    @FXML private ImageView rules_7;
+    @FXML private ImageView rules_8;
+    @FXML private ImageView rules_9;
+    @FXML private ImageView rules_10;
+    @FXML private ImageView rules_11;
+    @FXML private ImageView rules_12;
+    @FXML private ImageView rules_13;
+    @FXML private ImageView rules_14;
+    @FXML private ImageView rules_15;
 
     /**
      * This is for loading images to imageViews
@@ -21,6 +37,16 @@ public class RulesController implements Initializable {
      * */
     @Override
     public void initialize(URL location, ResourceBundle resources) {
-        firstSideRules.setImage(new Image(""));
+
+//        rules_0.setImage(new Image("client/resources/img/rules_map/rules-00.jpg"));
+//        rules_1.setImage(new Image("client/resources/img/rules_map/rules-01.jpg"));
+//        rules_2.setImage(new Image("client/resources/img/rules_map/rules-02.jpg"));
+//        rules_3.setImage(new Image("client/resources/img/rules_map/rules-03.jpg"));
+//        rules_4.setImage(new Image("client/resources/img/rules_map/rules-04.jpg"));
+//        rules_5.setImage(new Image("client/resources/img/rules_map/rules-05.jpg"));
+//        rules_6.setImage(new Image("client/resources/img/rules_map/rules-06.jpg"));
+//        rules_7.setImage(new Image("client/resources/img/rules_map/rules-07.jpg"));
+//        rules_8.setImage(new Image("client/resources/img/rules_map/rules-08.jpg"));
+//        rules_9.setImage(new Image("client/resources/img/rules_map/rules-09.jpg"));
     }
 }
