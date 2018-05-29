af54e2ab2d8b2cdae8cbd8d302171cf6cab6b892
Jan Douwe Sminia <jandouwesminia@gmail.com>
2018-05-29 16:32:39 +0200

    Merge + market villager factory

--- src/server/sources/models/villagers/VillagerFactory.java
+++ src/server/sources/models/villagers/VillagerFactory.java
@@ -27,63 +27,90 @@ public class VillagerFactory {
     }
 
     //TODO: afmaken methods
-    public Villager createVillager(ArrayList<Lanterns> lanterns){
+    public Villager createVillager(ArrayList<Lantern> lanterns){
         Villager villager = new Villager(lanterns, false, false );
         return villager;
     }
 
-    public BuilderVillager createBuilder(ArrayList<Lanterns> lanterns){
+    public BuilderVillager createBuilder(ArrayList<Lantern> lanterns){
         BuilderVillager villager = new BuilderVillager(lanterns, false, false);
         return villager;
     }
 
-    public TrainerVillager createTrainer(ArrayList<Lanterns> lanterns){
+    public TrainerVillager createTrainer(ArrayList<Lantern> lanterns){
         TrainerVillager villager = new TrainerVillager(lanterns, false, false);
         return villager;
     }
 
-    public AllroundVillager createAllround(ArrayList<Lanterns> lanterns){
+    public AllroundVillager createAllround(ArrayList<Lantern> lanterns){
         AllroundVillager villager = new AllroundVillager(lanterns, false, false);
         return villager;
     }
 
-    public ArrayList<Villager> createFromXml(){
+    public ArrayList<Villager> createFromXml() {
         list = new ArrayList<>();
 
         try {
             Document document = builder.parse(new InputSource(new FileReader(new File("src/server/resources/data/villagers.xml"))));
+            
+            NodeList villagers = document.getElementsByTagName("villager");
 
-            NodeList types = document.getElementsByTagName("type");
 
-            for (int i = 0; i < types.getLength(); i++){
-                Node typesNode = types.item(i);
-                switch (fetchType(typesNode).toLowerCase()) {
-                    case "builder" :
+            for (int i = 0; i < villagers.getLength(); i++) {
+                Node villagerNode = villagers.item(i);
+                Element villagerElement = (Element) villagerNode; 
+                
+                // Fetch villager type
+                switch (this.fetchType(villagerElement).toUpperCase()){
+                    case "BUILDER" : list.add(new BuilderVillager(fetchLanterns(villagerElement), false, false));
                         break;
-                    case "trainer" :
+                    case "TRAINER" : list.add(new TrainerVillager(fetchLanterns(villagerElement), false, false));
                         break;
-                    case "allround" :
+                    case "ALLROUND" : list.add(new AllroundVillager(fetchLanterns(villagerElement), false, false));
                         break;
-                    case "villager" :
+                    case "VILLAGER" : list.add(new Villager(fetchLanterns(villagerElement), false, false));
                         break;
                 }
+
             }
 
         } catch (IOException | SAXException e) {
             e.printStackTrace();
         }
 
+        System.out.println(list);
+
         return list;
 
     }
 
-    private String fetchType(Node typesNode){
+    private String fetchType(Element villager){
+        return villager.getElementsByTagName("type").item(0).getTextContent();
+    }
 
-        Element typesElement = (Element) typesNode;
+    private ArrayList<Lantern> fetchLanterns(Element villager) {
+        ArrayList<Lantern> lanterns = new ArrayList<Lantern>();
 
-        String type = typesElement.getElementsByTagName("text").item(0).getTextContent();
+        NodeList lanternsNode = villager.getElementsByTagName("lantern");
 
-        return type;
+        for (int i = 0; i < lanternsNode.getLength(); i++) {
+            Node lanternNode = lanternsNode.item(i);
+
+            lanterns.add(this.fetchLantern((Element) lanternNode));
+        }
+        
+        return lanterns;
+    }
+
+
+    private Lantern fetchLantern(Element lanternElement) {
+        // Fetch cost
+        int cost = Integer.parseInt(lanternElement.getElementsByTagName("cost").item(0).getTextContent());
+
+        // Fetch eyes
+        int eyes = Integer.parseInt(lanternElement.getElementsByTagName("eyes").item(0).getTextContent());
+
+        return new Lantern(cost, eyes);
     }
 
 }
