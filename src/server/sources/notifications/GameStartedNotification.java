767de1cc8aa2783311737b6b256801c1a9b0f9ae
Riezzie <richardkerkvliet@hotmail.com>
2018-05-31 11:54:35 +0200

    volgens mij niets gedaan

--- src/server/sources/notifications/GameStartedNotification.java
+++ src/server/sources/notifications/GameStartedNotification.java
@@ -8,7 +8,6 @@ import java.rmi.RemoteException;
 public class GameStartedNotification implements NotificationInterface {
     @Override
     public void execute(GameClientInterface gameClient) throws RemoteException {
-
         // Show main game client for everyone
         gameClient.getClient().showMain();
     }
