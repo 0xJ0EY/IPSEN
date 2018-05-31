d539f29e1e031d4dd9062d861c1cbad79348a93d
0xJ0EY <contact@joeyderuiter.me>
2018-05-31 12:49:09 +0200

    Added some JavaDocs to the main Server, still have to do a lot of code

--- src/server/sources/interfaces/ServerInterface.java
+++ src/server/sources/interfaces/ServerInterface.java
@@ -5,16 +5,47 @@ import java.rmi.RemoteException;
 
 public interface ServerInterface extends Remote {
 
+    /**
+     * Register an user at the game, also create a Player object in the GameController
+     * @param gameClientInterface the assigned gameClient
+     * @param username
+     * @throws RemoteException
+     */
     public void registerClient(GameClientInterface gameClientInterface, String username) throws RemoteException;
 
+    /**
+     * Remove an user from the game, also remove the player from the GameController
+     * @param gameClientInterface
+     * @throws RemoteException
+     */
     public void unregisterClient(GameClientInterface gameClientInterface) throws RemoteException;
 
+    /**
+     * Send an notification object to every connected client
+     * @param notification the NotificationInterface to be executed on the client
+     * @throws RemoteException
+     */
     public void notifyClients(NotificationInterface notification) throws RemoteException;
 
+    /**
+     *
+     * @param request
+     * @throws RemoteException
+     */
     public void executeRequest(RequestInterface request) throws RemoteException;
 
+    /**
+     * Execute an action object on the server and send the action notifcation to every client
+     * @param action
+     * @throws RemoteException
+     */
     public void executeAction(ActionInterface action) throws RemoteException;
 
+    /**
+     * Return the game controller, so the client can retrieve data via RMI
+     * @return GameControllerInterface
+     * @throws RemoteException
+     */
     public GameControllerInterface getGameController() throws RemoteException;
 
 }
