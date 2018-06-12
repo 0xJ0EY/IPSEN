85e95a4fc1e3d51ebac78f696da873f192d98132
Riezzie <richardkerkvliet@hotmail.com>
2018-06-12 16:40:24 +0200

    added javadoc villagers

--- src/server/sources/models/villagers/Buildable.java
+++ src/server/sources/models/villagers/Buildable.java
@@ -3,6 +3,10 @@ package server.sources.models.villagers;
 public interface Buildable {
 
     //TODO: implenting build
+
+    /**
+     * builds a house.
+     */
     void build();
 
 }
--- src/server/sources/models/villagers/Trainable.java
+++ src/server/sources/models/villagers/Trainable.java
@@ -3,6 +3,10 @@ package server.sources.models.villagers;
 public interface Trainable {
 
     //TODO: implenting train
+
+    /**
+     * trains a villager.
+     */
     void train();
 
 }
