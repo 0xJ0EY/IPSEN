f1b94f685febe297930b22f0b62abc1127795ddb
0xJ0EY <contact@joeyderuiter.me>
2018-06-12 14:35:36 +0200

    Added javadoc to server.strategies.villager

--- src/server/sources/strategies/villagers/AddVillagerStrategy.java
+++ src/server/sources/strategies/villagers/AddVillagerStrategy.java
@@ -4,8 +4,18 @@ import server.sources.interfaces.PlayerBoardInterface;
 
 import java.io.Serializable;
 
+/**
+ * Implementation of the stategy pattern for creating villagers.
+ * @author Joey de Ruiter
+ */
 public interface AddVillagerStrategy extends Serializable {
 
+    /**
+     * Add the villager to the playerboard.
+     * The reason for this strategy pattern is that system requires a local object to be set in the playerboard.
+     * @param playerboard
+     * @author Joey de Ruiter
+     */
     public void execute(PlayerBoardInterface playerboard);
 
 }
