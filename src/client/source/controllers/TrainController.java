52cb299916d87ca1f20da6308c43a508a604e715
Riezzie <richardkerkvliet@hotmail.com>
2018-06-15 15:37:42 +0200

    added keybinding to rest of the actions minus harvest

--- src/client/source/controllers/TrainController.java
+++ src/client/source/controllers/TrainController.java
@@ -9,6 +9,7 @@ import javafx.event.ActionEvent;
 import javafx.fxml.FXML;
 import javafx.scene.Parent;
 import javafx.scene.control.Button;
+import javafx.scene.input.KeyCode;
 import javafx.scene.layout.FlowPane;
 import javafx.scene.layout.GridPane;
 import server.sources.interfaces.MarketInterface;
@@ -135,6 +136,28 @@ public class TrainController implements ControllerInterface {
 
     public void onClickCancel() throws RemoteException{
         client.showMain();
+    }
+
+    @FXML public void keys() {
+        root.setOnKeyPressed(e -> {
+            KeyCode keyCode = e.getCode();
+            switch (keyCode) {
+                case T:
+                    try {
+                        this.onClickTrain();
+                    } catch (RemoteException e1) {
+                        e1.printStackTrace();
+                    }
+                    break;
+                case ESCAPE:
+                    try {
+                        this.onClickCancel();
+                    } catch (RemoteException e1) {
+                        e1.printStackTrace();
+                    }
+                    break;
+            }
+        });
     }
 
     public void updateButtons() {
